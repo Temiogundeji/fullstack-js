1ad5aecf644b50cbf04db3be2a66fca57b28dae7
Temiogundeji <yusuffogundeji@gmail.com>
2020-05-21 22:20:15 +0100

    auth.js file updated

--- controller/auth.js
+++ controller/auth.js
@@ -2,7 +2,6 @@ const passport = require('passport');
 const localStrategy = require('passport-local').Strategy;
 const SeqModel = require('../models/Config')
 const bcrypt = require('bcrypt');
-require('dotenv').config();
 const BCRYPT_SALT_R = 12;
 
 passport.use('signup', new localStrategy(
@@ -73,9 +72,11 @@ passport.use('login', new localStrategy({
 
 const JWTStrategy = require('passport-jwt').Strategy;
 const ExtractJWT = require('passport-jwt').ExtractJwt;
+require('dotenv').config();
 
+const secret = process.env.SECRET_KEY || 'secret';
 passport.use(new JWTStrategy({
-    secretOrKey: process.env.SECRET_KEY,
+    secretOrKey: secret,
     jwtFromRequest: ExtractJWT.fromUrlQueryParameter('secret_token')
 },(token, done) => {
     try {
