7e0695acf7dc960c9f0bac16e32d494a31fb98a5
Temiogundeji <yusuffogundeji@gmail.com>
2020-05-24 14:34:09 +0100

    auth.test.js file modified

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,19 +1,4 @@
-// it('should return status code 200 when valid data is received successfully!', (done) => { 
-//     const new_user = {
-//         email: "midetobi@gmail.com",
-//         password: "midetobi"
-//     };
-//     chai.request(app)
-//         .post('/user/signup')
-//         .send(new_user)
-//             .end((err, res) => {
-//             res.should.have.status(200);
-//             expect(res.body.message).to.equal('Signup successful');
-//             expect(res.body).to.have.property('session');
-//             expect(res).to.be.json;
-//             done();
-//     });
-// });
+
 // it('it should not create a user without email address', (done) => {
 //     let new_user = {
 //         email: "John",
@@ -28,3 +13,30 @@
 //         done();
 //       });
 // });
+const app = require('../app');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Signup tests', () => {
+    it('should return status code 200 when valid data is received successfully!', done => {
+        const new_user = {
+            email: "midetobi@gmail.com",
+            password: "midetobi"
+        };
+
+        chai.request(app)
+        .post('/user/signup')
+        .send(new_user)
+            .end((err, res) => {
+            res.should.have.status(200);
+            expect(res.body.message).to.equal('Signup successful');
+            expect(res.body).to.have.property('session');
+            expect(res).to.be.json;
+        });
+        done();
+    })
+});
